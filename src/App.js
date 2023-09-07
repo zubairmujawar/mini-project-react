55d151e73848e0914ffafca587a50e14b8ff2de3
zubairmujawar <139705672+zubairmujawar@users.noreply.github.com>
2023-09-07 20:22:06 +0530

    Landing page of Shoes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from "./components/Navbar"
+import './ModulCSS/navbar.css'
+import HeroSection from './components/HeroSection.js';
+import './ModulCSS/Hero.css'
+
+function App() {
+  return (
+  <>
+  <Navbar/>
+  <HeroSection/>
+  </>
+  );
+}
+
+export default App;
