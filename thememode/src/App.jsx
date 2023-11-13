9592c00307db36b1525ffc4b2a5b74534a4c9018
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2023-11-13 20:30:41 +0530

    theme mode completed

--- thememode/src/App.jsx
+++ thememode/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./Context/theme";
+import ThemeButton from "./Components/ThemeButton";
+import Card from "./Components/Card";
 
 
 function App() {
@@ -28,10 +30,10 @@ function App() {
      <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-        <button className="bg-gray-500 my-3 rounded-sm">This is button</button>
+       <ThemeButton/>
         </div>
         <div className="w-full max-w-sm mx-auto"></div>
-        {/* Card */}
+        <Card/>
       </div>
     </div> 
     </ThemeProvider>
