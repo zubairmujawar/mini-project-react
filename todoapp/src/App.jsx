42a59db3063d85c0aad3abae7ab6b04249ff039e
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2023-11-19 19:31:48 +0530

    todo working

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -2,27 +2,50 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [input, setinput] = useState("")
-  const [todos, settodos] = useState([])
+  const [input, setinput] = useState("");
+  const [items, setitems] = useState([]);
 
-  const handelSubmit = (e) => {
-    e.preventDefault()
-    console.log(input)
-    setinput('')
-  } 
-  const addItems =() => {
-    
-  }
-  
+  const addItems = (e) => {
+    e.preventDefault();
+    input === "" ? alert("ok") : setitems([...items, input]);
+    console.log(input);
+    setinput("");
+    // if(input == ""){
+    //   alert("Please write Todo")
+    // }else{
+    //   setitems([...items, input])
+    //   console.log(input)
+    //   setinput("")
+    // }
+  };
+
   return (
     <div className="flex main-div">
       <h1 className="text-3xl font-bold ">Todo App</h1>
       <form>
-        <input type="text" placeholder="write your todo" className=" border-e-red-300  border-black bg-slate-300 input" autoFocus value={input} onChange={handelSubmit}/>
-        <button onChange={(e)=>setinput(e.target.value)} className="btn" onClick={addItems}>Go</button>
-        <ul className="ul">
-          <li className="li" >This is a todo <span>Delete</span></li>
-        </ul>
+        <input
+          type="text"
+          placeholder="write your todo"
+          className=" border-e-red-300  border-black bg-slate-300 input"
+          autoFocus
+          value={input}
+          onChange={(e) => setinput(e.target.value)}
+        />
+        <button className="btn" onClick={addItems}>
+          Add
+        </button>
+        {
+        items.map((element, index) => {
+          return (
+            <ul className="ul" key={index}>
+              <li className="li">
+                {element}
+                <span>Delete</span>
+              </li>
+            </ul>
+          );
+        })
+        }
       </form>
     </div>
   );
